524811711757957d6053cbab8e55356d31e22984
Dragodec <140373828+Dragodec@users.noreply.github.com>
2026-05-07 17:49:28 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder encoded = new StringBuilder();
+        for(String str : strs)
+        {
+            encoded = encoded.append(str.length() + "#" + str);
+        }
+        return encoded.toString();
+    }
+
+    // 5#Hello5#World -> 0-13 (0 zero based index) (length=14)
+
+       
+
+    public List<String> decode(String str) {
+        List<String> list = new ArrayList<>();
+        int i = 0;
+        while(i < str.length())
+        {
+            int j = i;
+            while(str.charAt(j)!='#')
+            {
+                j++;
+            }
+            int length = Integer.parseInt(str.substring(i,j));
+            j++;
+            String word = str.substring(j, j+length);
+            list.add(word);
+            i = j + length;
+        }
+
+        return list;
+    }
+}
